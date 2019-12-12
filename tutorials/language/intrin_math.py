015a0e3e9ead235344ec2f572c80a0e91d398340
mjpsl <57522192+mjpsl@users.noreply.github.com>
2019-12-12 14:48:20 +0530

    Update intrin_math.py

--- tutorials/language/intrin_math.py
+++ tutorials/language/intrin_math.py
@@ -147,32 +147,7 @@ s[B].bind(bx, tvm.thread_axis("blockIdx.x"))
 s[B].bind(tx, tvm.thread_axis("threadIdx.x"))
 fcuda = tvm.build(s, [A, B], "cuda", name="mylog")
 print(fcuda.imported_modules[0].get_source())
-################### Adding pure intrinsic isfinite
 
-def isfinite(x):
-    """customized isfinite intrinsic function"""
-    return tvm.call_pure_intrin(x.dtype, "isfinite", x)
-
-def my_llvm_isfinite_rule(op):
-    """CUDA lowering rule for log"""
-    if op.dtype == "float32":
-        return tvm.call_pure_extern("float32", "isfinite", op.args[0])
-    elif op.dtype == "float64":
-        return tvm.call_pure_extern("float64", "isfinite", op.args[0])
-    else:
-        return op
-
-tvm.register_intrin_rule("llvm", "isfinite", my_llvm_isfinite_rule, override=True)
-n = tvm.var("n")
-A = tvm.placeholder((n,), name='A')
-B = tvm.compute(A.shape, lambda i: isfinite(A[i]), name="B")
-s = tvm.create_schedule(B.op)
-num_thread = 64
-bx, tx = s[B].split(B.op.axis[0], factor=num_thread)
-s[B].bind(bx, tvm.thread_axis("blockIdx.x"))
-s[B].bind(tx, tvm.thread_axis("threadIdx.x"))
-fcuda = tvm.build(s, [A, B], "llvm", name="isfinite")
-print(fcuda.imported_modules[0].get_source())
 
 ######################################################################
 # Summary
