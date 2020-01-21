61447f539f18f2a178bdaefb5744a7ff66b789ad
admins <madhusudan_jarhad@persistent.com>
2020-01-21 10:59:41 +0530

    removed duplicate test case

--- tests/python/frontend/tensorflow/test_forward.py
+++ tests/python/frontend/tensorflow/test_forward.py
@@ -201,7 +201,6 @@ def compare_tf_with_tvm(in_data, in_name, out_name, init_global_variables=False,
         sess.close()
 
 
-
 def is_gpu_available():
     from tensorflow.python.client import device_lib
     local_device_protos = device_lib.list_local_devices()
